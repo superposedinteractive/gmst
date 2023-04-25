6afc51be24b2862974f7390365c6cdb3cf9fe4fc
japan <49401941+japannt@users.noreply.github.com>
2023-04-25 13:34:52 +0200

    Shop UI

--- gamemodes/gmstation/entities/entities/npc_shop/cl_init.lua
+++ gamemodes/gmstation/entities/entities/npc_shop/cl_init.lua
@@ -3,12 +3,14 @@ include("shared.lua")
 local test_elements = {
 	{
 		["name"] = "Bench",
+		["desc"] = "A nice bench to sit on.",
 		["model"] = "models/props_c17/bench01a.mdl",
 		["price"] = 100
 	},
 	{
 		["name"] = "Table",
-		["model"] = "models/props_c17/tablecloth001.mdl",
+		["desc"] = "A nice table to put things on.",
+		["model"] = "models/props_c17/furnituretable002a.mdl",
 		["price"] = 100
 	}
 }
@@ -31,12 +33,13 @@ net.Receive("gmstation_store", function()
 	local music = CreateSound(LocalPlayer(), "gmstation/music/store.mp3")
 
 	timer.Simple(0.5, function()
+		music:Stop()
 		music:PlayEx(GLOBALS.volume, 100)
 	end)
 
 	timer.Create("gmstation_store_music", 60, 0, function()
 		music:Stop()
-		music:Play()
+		music:PlayEx(GLOBALS.volume, 100)
 	end)
 
 	GUIElements.store = vgui.Create("DFrame")
@@ -68,9 +71,28 @@ net.Receive("gmstation_store", function()
 		model:Dock(LEFT)
 		model:SetWide(128)
 		model:SetModel(k.model)
-		model:SetCamPos(Vector(100, 100, 100))
+		model:SetCamPos(Vector(100, 100, 50))
 		model:SetLookAt(Vector(0, 0, 0))
 		model:SetFOV(40)
 		model:SetAnimSpeed(20)
+
+		local buy = vgui.Create("DButton", item)
+		buy:Dock(RIGHT)
+		buy:DockMargin(32, 32, 32, 32)
+		buy:SetWide(128)
+		buy:SetText(k.price .. "cc")
+
+		local name = vgui.Create("DLabel", item)
+		name:Dock(TOP)
+		name:DockMargin(16, 16, 0, 0)
+		name:SetText(k.name)
+		name:SetFont("Trebuchet32")
+		name:SetTall(32)
+
+		local desc = vgui.Create("DLabel", item)
+		desc:Dock(TOP)
+		desc:DockMargin(16, 2, 0, 0)
+		desc:SetFont("Trebuchet8")
+		desc:SetText(k.desc)
 	end
 end)
