1a073017d92776ff5af7c6903a5824a3959f9035
japannt <49401941+japannt@users.noreply.github.com>
2023-05-01 17:46:21 +0200

    more rocketeers stuff, fixed some bugs in gmstbase

---
+++ gamemodes/rocketeers/gamemode/sh/teams.lua
@@ -0,0 +1,8 @@
+local spec_clr = Color(60, 60, 60)
+local red_clr = Color(220, 60, 60)
+local blue_clr = Color(60, 60, 220)
+
+team.SetUp(1, "Spectator", spec_clr)
+team.SetUp(2, "Red", red_clr)
+team.SetUp(3, "Blue", blue_clr)
+
